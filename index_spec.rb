669886857d4dc8b77268aef0d76ef5b19834eb28
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-05-05 22:47:36 -0700

    Add tests for Class constructor

--- index_spec.rb
+++ index_spec.rb
@@ -1,29 +1,42 @@
 require_relative 'spec_helper'
 require_relative 'index'
 
-RSpec.describe 'stock_picker()' do
-    it "returns [1,4] when called with [17,3,6,9,15,8,6,1,10]" do
-        exp = stock_picker([17,3,6,9,15,8,6,1,10])
-        res = [1,4]
-        expect(exp).to eq(res)
-    end
+RSpec.describe 'Board' do
+    describe 'initialise()' do
+        it 'returns a blank grid when called with no parameters' do
+            empty_board = Board.new
+            result = [
+                empty_board.pieces_placed,
+                empty_board.size,
+                empty_board.grid
+            ]
+            expected_grid = [
+                [Piece.EMPTY, Piece.EMPTY, Piece.EMPTY],
+                [Piece.EMPTY, Piece.EMPTY, Piece.EMPTY],
+                [Piece.EMPTY, Piece.EMPTY, Piece.EMPTY]
+            ]
+            expected = [0, 3, expected_grid]
+            expect(result).to eq(expected)
+        end
 
-    it "returns [1,4] when called with [7,1,5,3,6,4]" do
-        exp = stock_picker([7,1,5,3,6,4])
-        res = [1,4]
-        expect(exp).to eq(res)
-    end
+        it 'returns a non-empty grid when called with a custom non-empty 2D array' do
+            random_grid = [
+                [Piece.EMPTY, Piece.CIRCLE, Piece.CROSS],
+                [Piece.CROSS, Piece.CROSS, Piece.CROSS],
+                [Piece.CIRCLE, Piece.EMPTY, Piece.CIRCLE]
+            ]
+            custom_board = Board.new(random_grid)
+            result = [
+                custom_board.pieces_placed,
+                custom_board.size,
+                custom_board.grid
+            ]
+            expected_pieces_placed = 7
+            expected_grid = random_grid
+            expected = [7, 3, random_grid]
+            expect(result).to eq(expected)
+        end
 
-    it "returns [-1,-1] when called with [7,6,4,3,1]" do
-        exp = stock_picker([7,6,4,3,1])
-        res = [-1,-1]
-        expect(exp).to eq(res)
-    end
 
-    it "returns [-1,-1] when called with [1]" do
-        exp = stock_picker([1])
-        res = [-1,-1]
-        expect(exp).to eq(res)
     end
 end
-
